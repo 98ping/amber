aa406868a95d30e2df0befcfd43f270ec594a5ec
Max <67809373+98ping@users.noreply.github.com>
2023-11-06 22:00:46 -0800

    feat: add matrix studios maven repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.21"
+    `maven-publish`
 }
 
 group = "ltd.matrixstudios"
@@ -24,6 +25,27 @@ dependencies {
     testImplementation("com.github.Carleslc.Simple-YAML:Simple-Yaml:1.8.4")
     testImplementation("com.google.code.gson:gson:2.10.1")
     testImplementation(kotlin("test"))
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "matrixMaven"
+            url = uri("https://maven.matrixstudios.ltd/public")
+            credentials(PasswordCredentials::class)
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "ltd.matrixstudios"
+            artifactId = "amber"
+            version = "1.0.0"
+            from(components["java"])
+        }
+    }
 }
 
 tasks.test {
